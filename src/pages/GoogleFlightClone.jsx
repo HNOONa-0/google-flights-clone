8a713c7cbfff382f07e5877c29ce0270de6720b8
HNOONa-0 <hanyahmed11811@gmail.com>
2025-02-17 14:50:38 +0200

    add FlightResults component to display available flights and loading state in GoogleFlightClone

--- src/pages/GoogleFlightClone.jsx
+++ src/pages/GoogleFlightClone.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Search } from "lucide-react";
 import AirportSearchInput from "../components/AirportsSearchInput";
 import api from "../utils/api";
+import FlightResults from "../components/FlightResults";
 
 const GoogleFlightClone = () => {
   const [departure, setDeparture] = useState("");
@@ -10,6 +11,8 @@ const GoogleFlightClone = () => {
   const [nearbyAirports, setNearbyAirports] = useState([]);
   const [airports, setAirports] = useState([]);
   const [error, setError] = useState("");
+  const [avaialableFlights, setAvailableFlights] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const fromRef = useRef(null);
   const toRef = useRef(null);
@@ -24,8 +27,7 @@ const GoogleFlightClone = () => {
       setError("Please select both airports");
       return;
     }
-    console.log("fromAirport", fromAirport);
-    console.log("toAirport", toAirport);
+    setIsLoading(true);
 
     try {
       const response = await api("v2/flights/searchFlightsComplete", {
@@ -38,8 +40,11 @@ const GoogleFlightClone = () => {
         date: new Date().toISOString().split("T")[0],
       });
       console.log("response", response);
+      setAvailableFlights(response.data);
     } catch (error) {
       setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -49,79 +54,67 @@ const GoogleFlightClone = () => {
     }
   }, [error]);
   return (
-    <div className="container mx-auto p-6 bg-white shadow-xl rounded-lg max-w-xl border border-gray-200">
-      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
-        Flight Search
-      </h2>
+    <>
+      <div className="container mx-auto p-6 bg-white shadow-xl rounded-lg max-w-xl border border-gray-200">
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+          Flight Search
+        </h2>
 
-      <div className="flex flex-col space-y-4">
-        <label className="text-sm font-medium text-gray-700">
-          Cabin Class:
-          <select className="block w-full mt-1 p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300">
-            <option value="economy">Economy</option>
-            <option value="premium-economy">Premium Economy</option>
-            <option value="business">Business</option>
-            <option value="first">First</option>
-          </select>
-        </label>
-        <label className="text-sm font-medium text-gray-700">
-          Number of Adults:
-          <input
-            type="number"
-            ref={adultsRef}
-            min="1"
-            defaultValue="1"
-            className="block w-full mt-1 p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300"
-          />
-        </label>
-      </div>
-
-      <div className="mt-6 flex flex-col space-y-4">
-        <AirportSearchInput
-          ref={fromRef}
-          placeholder={"From"}
-          setError={setError}
-        />
-        <AirportSearchInput
-          ref={toRef}
-          placeholder={"To"}
-          setError={setError}
-        />
-        <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
-          <label className="flex-1 text-sm font-medium text-gray-700">
-            Departure Date:
-            <input
-              type="date"
-              value={departure}
-              onChange={(e) => setDeparture(e.target.value)}
-              className="block w-full mt-1 p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300"
-            />
+        <div className="flex flex-col space-y-4">
+          <label className="text-sm font-medium text-gray-700">
+            Cabin Class:
+            <select className="block w-full mt-1 p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300">
+              <option value="economy">Economy</option>
+              <option value="premium-economy">Premium Economy</option>
+              <option value="business">Business</option>
+              <option value="first">First</option>
+            </select>
           </label>
-          <label className="flex-1 text-sm font-medium text-gray-700">
-            Return Date:
+          <label className="text-sm font-medium text-gray-700">
+            Number of Adults:
             <input
-              type="date"
-              value={returnDate}
-              onChange={(e) => setReturnDate(e.target.value)}
+              type="number"
+              ref={adultsRef}
+              min="1"
+              defaultValue="1"
               className="block w-full mt-1 p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300"
             />
           </label>
         </div>
-      </div>
 
-      <button
-        className="mt-6 w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition flex items-center justify-center space-x-2"
-        onClick={searchFlights}
-      >
-        <Search size={16} />
-        <span>Search Flights</span>
-      </button>
-      {error && (
-        <p className="error-message text-red-500 text-sm mt-4 text-center">
-          {error}
-        </p>
-      )}
-    </div>
+        <div className="mt-6 flex flex-col space-y-4">
+          <AirportSearchInput
+            ref={fromRef}
+            placeholder={"From"}
+            setError={setError}
+          />
+          <AirportSearchInput
+            ref={toRef}
+            placeholder={"To"}
+            setError={setError}
+          />
+        </div>
+
+        <button
+          className="mt-6 w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition flex items-center justify-center space-x-2"
+          onClick={searchFlights}
+        >
+          <Search size={16} />
+          <span>Search Flights</span>
+        </button>
+        {error && (
+          <p className="error-message text-red-500 text-sm mt-4 text-center">
+            {error}
+          </p>
+        )}
+      </div>
+      <div className="w-full mt-8">
+        <h3 className="text-xl font-semibold text-center text-gray-800">
+          <FlightResults isLoading={isLoading} flights={avaialableFlights} />
+          <FlightResults isLoading={isLoading} flights={avaialableFlights} />
+        </h3>
+      </div>
+    </>
   );
 };
 
